ae9ff6f23268a8744d72f483d6293e229d97ab52
saibhvani <145922824+saibhvani@users.noreply.github.com>
2023-09-26 16:14:48 +0530

    Add files via upload

---
+++ stringappend.py
@@ -0,0 +1,67 @@
+#capitalize
+data = "iam saibhavani"
+print(data.capitalize())
+
+#title
+name = "sai bhavani"
+print(name.title())
+
+#upper case
+name = "motamarri sai bhavani"
+print(name.upper())
+
+#lower case
+name = "MOTAMARRI SAI BHAVANI"
+print(name.lower())
+
+#case fold
+name = "SAI BHAVANI"
+print(name.casefold())
+
+#swap case
+name = "saibhavani"
+print(name.swapcase())
+
+#isnumeric
+a = "1234"
+print(a.isnumeric())
+
+#isalnumeric
+a = "saibhavani123"
+print(a.isalnum())
+
+#isdecimal
+a = "2345"
+print(a.isdecimal())
+
+#isdigit
+a = "sai bhavani153"
+print(a.isdigit())
+
+#isascil
+a = "saibhavani"
+print(a.isascii())
+
+#isupper
+a = "saibhavani"
+print(a.isupper())
+
+#islower
+a = "SAIBHAVANI"
+print(a.islower())
+
+#isspace
+a = "saibhavani"
+print(a.isspace())
+
+#isidentifier
+a = "saibhavani"
+print(a.isidentifier())
+
+#isprintable
+a = "saibhavani"
+print(a.isprintable())
+
+#istitle
+a = " saibhavani "
+print(a.istitle())
